f4d4f14fd31da87049caf004a401f60ceba55d43
Flaviohmm <fhenrique609@gmail.com>
2021-05-18 14:12:37 -0300

    Criando o redirecionamento

--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from devpro.encurtador.models import UrlRedirect
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
-def redirecionar(requisicao):
-    pass
+def redirecionar(requisicao, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    return redirect(url_redirect.destino)
