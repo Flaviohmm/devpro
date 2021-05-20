44b86fe462db9d63a90013c1962d7e9930f43c8a
Flaviohmm <fhenrique609@gmail.com>
2021-05-20 14:44:23 -0300

    Implementado Sistema de logs de acesso

--- devpro/encurtador/admin.py
+++ devpro/encurtador/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from devpro.encurtador.models import UrlRedirect
+from devpro.encurtador.models import UrlLog, UrlRedirect
 
 @admin.register(UrlRedirect)
 class UrlRedirect(admin.ModelAdmin):
     list_display = ('destino', 'slug', 'criado_em', 'atualizado_em')
+
+@admin.register(UrlLog)
+class UrlLogAdmin(admin.ModelAdmin):
+    list_display = ('origem', 'criado_em', 'user_agent', 'host', 'ip', 'url_redirect')
+
+    def has_changed_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
+    def has_add_permission(self, request):
+        return False
--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -1,4 +1,4 @@
-from devpro.encurtador.models import UrlRedirect
+from devpro.encurtador.models import UrlLog, UrlRedirect
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 
@@ -15,4 +15,11 @@ def relatorios(requisicao, slug):
 
 def redirecionar(requisicao, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
+    UrlLog.objects.create(
+        origem = requisicao.META.get('TTP_REFERER'),
+        user_agent = requisicao.META.get('HTTP_USER_AGENT'),
+        host = requisicao.META.get('HTTP_HOST'),
+        ip = requisicao.META.get('REMOTE_ADDR'),
+        url_redirect = url_redirect
+    )
     return redirect(url_redirect.destino)
